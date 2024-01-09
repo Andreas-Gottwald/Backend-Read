89f2cae76dc59207b977dbe328290966068d5c1f
Andreas Gottwald <andreas.gottwald@hotmail.de>
2024-01-09 22:17:36 +0100

    finish

--- components/Product/index.js
+++ components/Product/index.js
@@ -1,7 +1,39 @@
+// import useSWR from "swr";
+// import { useRouter } from "next/router";
+// import { ProductCard } from "./Product.styled";
+// import { StyledLink } from "../Link/Link.styled";
+
+// export default function Product() {
+//   const router = useRouter();
+//   const { id } = router.query;
+
+//   const { data, isLoading } = useSWR(`/api/products/${id}`);
+
+//   if (isLoading) {
+//     return <h1>Loading...</h1>;
+//   }
+
+//   if (!data) {
+//     return;
+//   }
+
+//   return (
+//     <ProductCard>
+//       <h2>{data.name}</h2>
+//       <p>Description: {data.description}</p>
+//       <p>
+//         Price: {data.price} {data.currency}
+//       </p>
+//       <StyledLink href="/">Back to all</StyledLink>
+//     </ProductCard>
+//   );
+// }
+
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
+import Comments from "../Comments";
 
 export default function Product() {
   const router = useRouter();
@@ -24,6 +56,7 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
